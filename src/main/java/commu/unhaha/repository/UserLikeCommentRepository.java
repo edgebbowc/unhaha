8ce63f1a9cdc5056b08ac4e33b0aca8d9afe4fe9
Juyong <edgebbowc@naver.com>
2025-06-03 00:08:22 +0900

    :sparkles: feat: 사용자별 좋아요 댓글 조회 기능 추가
    - findByUserId() 메서드 신규 추가
    - 특정 사용자가 좋아요한 모든 댓글 목록 조회
    - 댓글 목록에서 좋아요 상태 표시 기능 지원

--- src/main/java/commu/unhaha/repository/UserLikeCommentRepository.java
+++ src/main/java/commu/unhaha/repository/UserLikeCommentRepository.java
@@ -3,8 +3,13 @@ package commu.unhaha.repository;
 import commu.unhaha.domain.UserLikeComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Arrays;
+import java.util.List;
+
 public interface UserLikeCommentRepository extends JpaRepository<UserLikeComment, Long> {
     boolean existsByCommentIdAndUserId(Long articleId, Long userId);
 
     void deleteByCommentIdAndUserId(Long articleId, Long userId);
+
+    List<UserLikeComment> findByUserId(Long userId);
 }
