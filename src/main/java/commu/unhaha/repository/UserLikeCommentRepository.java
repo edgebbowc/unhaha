1b18420b96275e0776d9598d61f2871f5de3c4f6
Juyong <edgebbowc@naver.com>
2025-06-04 16:18:01 +0900

    :pencil2: fix: UserLikeCommentRepository 파라미터명 수정
    Long articleId → Long commentId로 올바른 파라미터명 적용

--- src/main/java/commu/unhaha/repository/UserLikeCommentRepository.java
+++ src/main/java/commu/unhaha/repository/UserLikeCommentRepository.java
@@ -7,9 +7,9 @@ import java.util.Arrays;
 import java.util.List;
 
 public interface UserLikeCommentRepository extends JpaRepository<UserLikeComment, Long> {
-    boolean existsByCommentIdAndUserId(Long articleId, Long userId);
+    boolean existsByCommentIdAndUserId(Long commentId, Long userId);
 
-    void deleteByCommentIdAndUserId(Long articleId, Long userId);
+    void deleteByCommentIdAndUserId(Long commentId, Long userId);
 
     List<UserLikeComment> findByUserId(Long userId);
 }
